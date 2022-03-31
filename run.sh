1ba0f18afa710e64b7040802001e2b7fe4ef86b6
howard-quantrend <howard@quantrend.ai>
2022-03-31 14:36:28 +0000

    Modify

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 sudo apt-get update
 sudo apt install python3-pip -y
-wget https://github.com/yuya-takeyama/percentile/releases/download/v0.0.1/linux_amd64_0.0.1.zip -o percentile.zip
-unzip percentile.zip
-wget https://github.com/yuya-takeyama/ntimes/releases/download/v0.1.0/linux_amd64_0.1.0.zip -o ntimes.zip
-unzip ntimes.zip
-./ntimes -- curl https://api.binance.com/api/v3/ping -s -o /dev/null -w  "%{time_starttransfer}\n" | ./percentile
+pip3 install numpy scipy requests
+wget https://raw.githubusercontent.com/howard-quantrend/latency/main/latency.py
+python3 latency.py
